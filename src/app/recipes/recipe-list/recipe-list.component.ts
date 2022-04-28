320efdac39ca888474f806bb64a919bc091f212c
Diego Rojas <diego-fernando.rojas@getronics.com>
2022-04-28 10:43:20 +0200

    Creating a Recipe Model, Getting Recipe data (ngFor), recipe-detail, recipe-list & shoopingListComponent layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
     selector: 'app-recipe-list',
@@ -6,4 +7,12 @@ import { Component } from "@angular/core";
     styleUrls: ['./recipe-list.component.css']
 })
 
-export class RecipeListComponent {}
+export class RecipeListComponent {
+    //Array of recipe ../recipe.model.ts
+    recipes: Recipe[] = [
+        new Recipe('A test recipe','This is simply a test',
+        'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg'),
+        new Recipe('A test recipe','This is simply a test',
+        'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg')
+    ];
+}
