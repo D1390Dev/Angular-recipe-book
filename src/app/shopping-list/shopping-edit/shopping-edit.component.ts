019d707c1918109ac0109488a9d03bf82703bf14
Diego Rojas <diego-fernando.rojas@getronics.com>
2022-05-01 12:43:38 +0200

    Add Elements to a list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameIngredientInput') nameInputRef: ElementRef;
+  @ViewChild('amountIngredientInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addItem(){
+    const ingName = this.nameInputRef.nativeElement.value;
+    const amoName = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, amoName);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
