cb1af6eb242f22e985fe8ba30d3675e264eb2c4d
Diego Rojas <diego-fernando.rojas@getronics.com>
2022-05-13 15:06:51 +0200

    FORM(Reactive Template). Adding the Recip Edit Form

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription } from "rxjs";
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipes.service";
 
@@ -9,19 +10,30 @@ import { RecipeService } from "../recipes.service";
     styleUrls: ['./recipe-list.component.css']
 })
 
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
     //Array of recipe ../recipe.model.ts
     recipes: Recipe[];
+    subscription: Subscription;
 
     constructor(private recipeService: RecipeService,
                 private router: Router,
                 private route: ActivatedRoute) { }
 
     ngOnInit(): void {
+        this.subscription = this.recipeService.recipesChanged
+        .subscribe(
+            (recipes: Recipe[]) => {
+                this.recipes = recipes;
+            }
+        );
         this.recipes = this.recipeService.getRecipes();
     }
 
     onNewRecipe() {
         this.router.navigate(['new'], {relativeTo: this.route});
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
     }
 }
