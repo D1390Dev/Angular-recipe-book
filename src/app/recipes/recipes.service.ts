fcf30e55fe0d0fac9a0b19d1c1af60b44b45a55d
Diego Rojas <diego-fernando.rojas@getronics.com>
2022-05-18 12:03:59 +0200

    HTTP Requests

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -8,28 +8,34 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
     // recipeSelected = new Subject<Recipe>();
     recipesChanged = new Subject<Recipe[]>();
+    private recipes: Recipe[] = [];
 
     //Array of recipe ../recipe.model.ts
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Beans', 
-            'This is an amazing soup',
-            'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg',
-            [
-                new Ingredient ('Meat', 1),
-                new Ingredient ('French Fries', 5)
-            ]),
-        new Recipe(
-            'Burger',
-            'This is a delicious burger',
-            'https://sevilla.abc.es/contenidopromocionado/wp-content/uploads/sites/2/2019/09/rrss-wp-burguer.jpeg',
-            [
-                new Ingredient ('Buns', 2),
-                new Ingredient ('Meat', 3)
-            ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Beans', 
+    //         'This is an amazing soup',
+    //         'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg',
+    //         [
+    //             new Ingredient ('Meat', 1),
+    //             new Ingredient ('French Fries', 5)
+    //         ]),
+    //     new Recipe(
+    //         'Burger',
+    //         'This is a delicious burger',
+    //         'https://sevilla.abc.es/contenidopromocionado/wp-content/uploads/sites/2/2019/09/rrss-wp-burguer.jpeg',
+    //         [
+    //             new Ingredient ('Buns', 2),
+    //             new Ingredient ('Meat', 3)
+    //         ])
+    // ];
 
     constructor(private slService: ShoppingListService){}
+    
+    setRecipes(recipes: Recipe[]){
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes(){
         return this.recipes.slice();//Devuelve una copia exacta por lo que no se puede acceder al dato original
